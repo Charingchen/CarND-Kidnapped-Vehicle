fb6111857ae09fe97bc5c00afc1d667f974f2b75
Charing Chen <charinghaxen@gmail.com>
2020-09-10 15:13:40 -0700

    Add VS windows Config
    intellisense note working

--- src/particle_filter.cpp
+++ src/particle_filter.cpp
@@ -20,6 +20,7 @@
 
 using std::string;
 using std::vector;
+using std::normal_distribution;
 
 void ParticleFilter::init(double x, double y, double theta, double std[]) {
   /**
@@ -30,7 +31,16 @@ void ParticleFilter::init(double x, double y, double theta, double std[]) {
    * NOTE: Consult particle_filter.h for more information about this method 
    *   (and others in this file).
    */
-  num_particles = 0;  // TODO: Set the number of particles
+  num_particles = 50;  // TODO: Set the number of particles
+  std::default_random_engine gen;
+  normal_distribution<double> dist_x(x,std[0]);
+  normal_distribution<double> dist_y(y,std[1]);
+  normal_distribution<double> dist_theta(theta,std[2]);
+
+  for (int i = 0; i < num_particles; ++i){
+    Particle temp_particle 
+  }
+
 
 }
 
